5a5efe95bc1f50c65a83abaa3d2de137b0f5bed0
ShrapitAatma <sabiyogi19@gmail.com>
2020-06-05 22:40:43 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends React.Component {
+  render() {
+    return <h1>Hello from the tech store</h1>
+  }
+}
+
+export default App;
